8bd7c7fc6261c530a266c59d7b6941de958406e4
Valocode <valentin.seiler1@gmail.com>
2025-06-03 22:55:32 +0200

    ajout image

--- backend/game_roles.py
+++ backend/game_roles.py
@@ -4,23 +4,27 @@ roles_catalog = [
         "name": "Loup-Garou",
         "description": "Elimine un villageois chaque nuit.",
         "type": "loup",
+        "image": '/roles/loup.png',
     },
     {
         "id": "voyante",
         "name": "Voyante",
         "description": "Peut découvrir le rôle d’un joueur chaque nuit.",
         "type": "village",
+        "image": "/roles/voyante.png",
     },
     {
         "id": "cupidon",
         "name": "Cupidon",
         "description": "Désigne 2 amoureux pour la partie entière.",
         "type": "village",
+        "image": "/roles/cupidon.png",
     },
     {
         "id": "villageois",
         "name": "Villageois",
         "description": "Pas de pouvoir particulier.",
         "type": "village",
+        "image": "/roles/villageois.png",
     },
 ]
