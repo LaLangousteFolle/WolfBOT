e928311f78006fe545e0a00213688f8a34c3e38c
Valocode <valentin.seiler1@gmail.com>
2025-06-03 22:30:43 +0200

    add game_roles.py

---
+++ backend/game_roles.py
@@ -0,0 +1,26 @@
+roles_catalog = [
+    {
+        "id": "loup",
+        "name": "Loup-Garou",
+        "description": "Elimine un villageois chaque nuit.",
+        "type": "loup",
+    },
+    {
+        "id": "voyante",
+        "name": "Voyante",
+        "description": "Peut découvrir le rôle d’un joueur chaque nuit.",
+        "type": "village",
+    },
+    {
+        "id": "cupidon",
+        "name": "Cupidon",
+        "description": "Désigne 2 amoureux pour la partie entière.",
+        "type": "village",
+    },
+    {
+        "id": "villageois",
+        "name": "Villageois",
+        "description": "Pas de pouvoir particulier.",
+        "type": "village",
+    },
+]
