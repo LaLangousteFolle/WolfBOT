c23adb03654f01803226164e6d9993a9491cc5fd
Valocode <valentin.seiler1@gmail.com>
2025-06-04 20:09:12 +0200

    deploy

--- backend/endpoints/game.py
+++ backend/endpoints/game.py
@@ -1,23 +1,4 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from websocket import router as ws_router
-from game_logic import start_game, reset_game
 from game_roles import roles_catalog
-from auth import get_current_user
-from fastapi import Depends, HTTPException
-from game_state import game_state
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(ws_router)
 
 @app.post("/start")
 async def start_route(current_user: dict = Depends(get_current_user)):
@@ -36,4 +17,3 @@ async def my_role(current_user: dict = Depends(get_current_user)):
 @app.get("/roles")
 async def roles():
     return roles_catalog
-
