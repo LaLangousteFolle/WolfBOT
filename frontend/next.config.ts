35cb8ff69d429908194149beaf974fe8002495fb
ValCryptoCode <valentin.seiler1@gmail.com>
2025-06-06 13:20:34 +0200

    add domain next

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn.discordapp.com", "b1157417.smushcdn.com"],
+    domains: ["cdn.discordapp.com", "b1157417.smushcdn.com","upload.wikimedia.org"],
   },
   /* config options here */
 };
