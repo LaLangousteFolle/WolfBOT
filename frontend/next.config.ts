09a60790ada5ab324f2b874a8561666c39dcacf9
ValCryptoCode <valentin.seiler1@gmail.com>
2025-06-06 14:22:20 +0200

    #

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn.discordapp.com", "b1157417.smushcdn.com","upload.wikimedia.org"],
+    domains: ["cdn.discordapp.com", "b1157417.smushcdn.com","upload.wikimedia.org",'wolfbot-production.up.railway.app'],
   },
   /* config options here */
 };
